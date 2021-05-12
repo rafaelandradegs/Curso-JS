38137f859d8a29c7ae9686368271c1ba911f07e5
rafaelandradegs <rafaelandradegs@gmail.com>
2021-05-12 15:36:05 -0300

    Condições Parte 1

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro e ${vel}km por hora`)
+if (vel > 60){ //Consição Simples!
+    console.log(`Voce ultrapassou a velocidade permitida MUTADO`)
+}
+    console.log(`Dirija sempre com cinto de seguranca!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Reino Unido'
+console.log(`vivendo em ${pais}`)
+if (pais != 'Brasil') {
+    console.log('Voce e Estrangeiro!')
+}else {
+    console.log('Voce e Brasileiro!')
+}
