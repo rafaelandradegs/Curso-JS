7289dae6823e8f54d3f01bf6195c90f5e8cad289
rafaelandradegs <rafaelandradegs@gmail.com>
2021-05-14 08:42:46 -0300

    Exercicio Gerador de Tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,15 @@
-alert('ola')
+function gerar() {
+    var nt = document.getElementById('ntabuada')
+    var resposta = document.getElementById('seltab')
+    if(nt.value.length == 0) {
+        alert('Impossivel gerar Tabuada sem um  numero.')
+    } else {
+        resposta.innerHTML = ''
+        for(var i = 1; i <= 10; i++) {
+            var item = document.createElement('option')
+            item.text = `${nt.value} x ${i} = ${nt.value * i}`
+            item.value = `resposta${i}`
+            resposta.appendChild(item)
+        }
+    }
+}
